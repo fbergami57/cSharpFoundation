f9bcc3820a3a64aa13823074e5d31a431ab63932
fbergami57 <fabiobergami57@gmail.com>
2025-04-24 23:26:25 -0300

    BergamiDell correção ExerciseBool

---
+++ Exercise - Complete a challenge to combine string array values as strings and as integers/ExerciseBool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number;
+    if (decimal.TryParse(value, CultureInfo.InvariantCulture, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total:F2}");
